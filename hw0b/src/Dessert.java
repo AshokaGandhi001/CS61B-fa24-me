ca1718ea1fea35182660d0d139dc8eb5e392b205
AshokaGandhi001 <ashokagandhi@outlook.com>
2025-10-11 18:17:42 +0800

    complete.

--- hw0b/src/Dessert.java
+++ hw0b/src/Dessert.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Dessert {
+    public int flavor;
+    public int price;
+    public static int numDesserts = 0;
+
+    public Dessert(int flavor, int price){
+        this.flavor = flavor;
+        this.price = price;
+        Dessert.numDesserts += 1;
+    }
+
+    public void printDessert(){
+        System.out.print(this.flavor + " ");
+        System.out.print(this.price + " ");
+        System.out.println(Dessert.numDesserts);
+    }
+    public static void main(String[] args){
+        System.out.println("I love dessert!");
+    }
 }
