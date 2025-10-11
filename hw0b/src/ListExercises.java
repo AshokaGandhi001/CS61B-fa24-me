d74e08965f9768b93c45c36a58877dc8683e7526
AshokaGandhi001 <ashokagandhi@outlook.com>
2025-10-11 16:44:29 +0800

    complete all

--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -28,13 +28,27 @@ public class ListExercises {
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
         // TODO: Fill in this function.
-        return null;
+        List<Integer> common_item = new ArrayList<>();
+        for (int number : L1){
+            if (L2.contains(number)){
+                common_item.add(number);
+            }
+        }
+        return common_item;
     }
 
 
     /** Returns the number of occurrences of the given character in a list of strings. */
     public static int countOccurrencesOfC(List<String> words, char c) {
         // TODO: Fill in this function.
-        return 0;
+        int count = 0;
+        for (String current_word : words){
+            for (int i = 0; i < current_word.length(); i ++){
+                if(c == current_word.charAt(i)){
+                    count += 1;
+                }
+            }
+        }
+        return count;
     }
 }
