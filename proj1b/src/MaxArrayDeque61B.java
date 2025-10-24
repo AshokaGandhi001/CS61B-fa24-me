4de4cd8ff81a1f4b08fc0fbb3633dd7652eb248f
AshokaGandhi001 <ashokagandhi@outlook.com>
2025-10-24 11:47:51 +0800

    create and implement this class

--- proj1b/src/MaxArrayDeque61B.java
+++ proj1b/src/MaxArrayDeque61B.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.Comparator;
+public class MaxArrayDeque61B<T> extends ArrayDeque61B<T> {
+    private Comparator<T> comp;
 
-public class MaxArrayDeque61B {
+    public MaxArrayDeque61B(Comparator<T> comp) {
+        super();
+        this.comp = comp;
+    }
+    public T max() {
+        return max(comp);
+    }
+
+    public T max(Comparator<T> c) {
+        if (this.isEmpty()) return null;
+        T maxItem = this.get(0);
+        for (T item : this) {
+            if (c.compare(item, maxItem) > 0) {
+                maxItem = item;
+            }
+        }
+        return maxItem;
+    }
+
 }
