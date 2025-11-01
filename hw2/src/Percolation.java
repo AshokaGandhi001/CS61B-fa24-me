b61ed3d7107b55231c0b5f52951159ffd9fa519b
AshokaGandhi001 <ashokagandhi@outlook.com>
2025-11-01 11:09:26 +0800

    complete constructor

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -1,11 +1,21 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-
 public class Percolation {
     // TODO: Add any necessary instance variables.
+    int boardSize;
+    boolean[][] universe;
+    WeightedQuickUnionUF unionUF;
 
     public Percolation(int N) {
         // TODO: Fill in this constructor.
+        boardSize = N;
+        universe = new boolean[boardSize][boardSize];
+        for (int r = 0; r < boardSize; r++) {
+            for (int c = 0; c < boardSize; c++) {
+                universe[r][c] = false;
+            }
+        }
+        unionUF = new WeightedQuickUnionUF(boardSize);
     }
 
     public void open(int row, int col) {
@@ -24,6 +34,7 @@ public class Percolation {
 
     public int numberOfOpenSites() {
         // TODO: Fill in this method.
+
         return 0;
     }
 
@@ -34,5 +45,7 @@ public class Percolation {
 
     // TODO: Add any useful helper methods (we highly recommend this!).
     // TODO: Remove all TODO comments before submitting.
-
+    public int xyTo1D(int r, int c) {
+        return c*boardSize+r;
+    }
 }
