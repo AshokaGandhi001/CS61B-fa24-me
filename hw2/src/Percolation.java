d68562befd90ffd966ee38a4db8edf801e151fd8
AshokaGandhi001 <ashokagandhi@outlook.com>
2025-11-03 10:47:19 +0800

    add idvalid and virtual top and bottom.

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -6,6 +6,8 @@ public class Percolation {
     boolean[][] universe;
     WeightedQuickUnionUF unionUF;
     int numberOfOpenSite = 0;
+    int top;
+    int bottom;
 
     public Percolation(int N) {
         // TODO: Fill in this constructor.
@@ -16,13 +18,27 @@ public class Percolation {
                 universe[r][c] = false;
             }
         }
-        unionUF = new WeightedQuickUnionUF(boardSize*boardSize);
+        //create extra two boxes to put virtual top and bottom.
+        unionUF = new WeightedQuickUnionUF(boardSize*boardSize +2);
+        top = boardSize*boardSize;
+        bottom = top + 1;
     }
 
     public void open(int row, int col) {
         // TODO: Fill in this method.
+        if (!isValid(row, col)) {
+            throw new IllegalArgumentException("row or col index out of bounds");
+        }
+
+        int siteIndex = xyTo1D(row, col);
         universe[row][col] = true;
         numberOfOpenSite++;
+
+        if (row == 0) unionUF.union(siteIndex, top);
+        if (row == boardSize) unionUF.union(siteIndex, bottom);
+
+        int[][] dir = {{1, 0}, {-1,0}, {0, 1}, {0, -1}};
+
     }
 
     public boolean isOpen(int row, int col) {
@@ -49,5 +65,8 @@ public class Percolation {
     // TODO: Remove all TODO comments before submitting.
     public int xyTo1D(int r, int c) {
         return c*boardSize+r;
+    }
+    public boolean isValid(int row, int col) {
+        return (row >= 0 && row < boardSize && col >= 0 && col < boardSize);
     }
 }
